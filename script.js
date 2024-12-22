acbc9cce4e7310234dd7ce569659b1e486ef49d2
bhumit <bhumitsachaniya1010@gmail.com>
2024-12-22 17:44:24 +0530

    null

--- script.js
+++ script.js
@@ -4,14 +4,42 @@ const apiurl = "https://api.openweathermap.org/data/2.5/weather"
 
 
 async function CheckWeather() {
+
     const city = document.getElementById("city").value;
     const response = await fetch(apiurl + `?q=${city}` + `&units=metric&appid=${apikey}`);
     let data = await response.json();
+    console.log(data);
+    console.log(data.status);
+    
 
-    if (!data) return;
+  
     document.querySelector("#cityName").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humiditylevel").innerHTML = data.main.humidity + "%";
     document.querySelector(".windspeed").innerHTML = data.wind.speed + "km/h";
 
+    switch(data.weather[0].main)
+    {
+        case "Clear":
+            document.querySelector("#wetherImges").src = "/imgs/sun.png";
+         
+            break;
+        case "Clouds":
+            document.querySelector("#wetherImges").src = "/imgs/cloud.png";
+            break;
+        case "Rain":
+            document.querySelector("#wetherImges").src = "/imgs/rain.png";
+          
+            break;
+        case "Drizzle":
+           
+            break;
+        case "Thunderstorm":
+            document.querySelector("#wetherImges").src = "/imgs/storm.png";
+            break;
+        default:
+            document.querySelector("#wetherImges").src = "/imgs/404.png";
+
+    }
 }
+
