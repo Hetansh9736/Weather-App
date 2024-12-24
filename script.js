fe9988d3ec6e39cbf39be1442f330f802067fac1
Hetansh <hetanshsachaniya07@gmail.com>
2024-12-24 11:35:53 +0530

    Image fixed

--- script.js
+++ script.js
@@ -7,53 +7,31 @@ async function CheckWeather() {
 
     const city = document.getElementById("text").value;
     const response = await fetch(apiurl + `?q=${city}` + `&units=metric&appid=${apikey}`);
+    const weathericon = document.querySelector("#wetherImges");
     let data = await response.json();
     console.log(data);
     console.log(data.status);
-    
-
-  
     document.querySelector("#cityName").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humiditylevel").innerHTML = data.main.humidity + "%";
     document.querySelector(".windspeed").innerHTML = data.wind.speed + "km/h";
 
-    switch(data.weather[0].main)
-    {
-        case "Clear":
-            document.querySelector("#wetherImges").src = "/imgs/sun.png";
-         
-            break;
-        case "Clouds":
-            document.querySelector("#wetherImges").src = "/imgs/cloud.png";
-            break;
-        case "Rain":
-            document.querySelector("#wetherImges").src = "/imgs/rain.png";
-          
-            break;
-        case "Drizzle":
-           
-            break;
-        case "Thunderstorm":
-            document.querySelector("#wetherImges").src = "/imgs/storm.png";
-            break;
-        default:
-            document.querySelector("#wetherImges").src = "/imgs/404.png";
+    let cityName = document.getElementsByClassName("text")
+    document.addEventListener("DOMContentLoaded", () => {
+        cityName[0].addEventListener("keyup", function (e) {
+            if (e.key === "Enter") {
+                CheckWeather();
+            }
+        })
+    });
 
+    if (data.weather[0].main === "Clouds") {
+        weathericon.src = "imgs/cloud.png";
+    } else if (data.weather[0].main === "Rain") {
+        weathericon.src = "imgs/rain.png";
+    } else if (data.weather[0].main === "Clear") {
+        weathericon.src = "imgs/sun.png";
+    } else if (data.weather[0].main === "Snow") {
+        weathericon.src = "imgs/storm.png";
     }
 }
-
-let cityName = document.getElementsByClassName("text")
-document.addEventListener("DOMContentLoaded",()=>{
-    cityName[0].addEventListener("keyup", function(e) {
-        if(e.key === "Enter")
-        {
-            CheckWeather();
-
-        }
-    })
-    
-    
-    
-    
-})
